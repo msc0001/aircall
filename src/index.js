20ac247af464d3d28fd59e1ddfbdeb3ba2a93cfa
Mangal <mangalsinghchouhan786@gmail.com>
2024-01-04 14:58:22 +0530

    base structure updated

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import './css/body.css';
-import './css/app.css';
-import './css/header.css';
+import './Css/body.css';
+import './Css/app.css';
+import './Css/header.css';
 
 import App from './App.jsx';
 
